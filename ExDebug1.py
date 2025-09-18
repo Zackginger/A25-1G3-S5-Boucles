18c778cf16e580b02fcd9a8876089c3ac755098c
Zackginger <2551478@cegepoutaouais.qc.ca>
2025-09-18 11:12:25 -0400

    inputs

--- ExDebug1.py
+++ ExDebug1.py
@@ -43,4 +43,7 @@ def environnement_optimal(temp, poussiere, humidite):
 
 
 if __name__ == "__main__":
-    print(environnement_optimal(25, "faible", 40))
+    temp=float(input("entrer la température: "))
+    poussière=input("entrer le niveau de poussière: ")
+    humidite=float(input("quelle est l'humidité: "))
+    print(environnement_optimal(temp, poussière, humidite))
